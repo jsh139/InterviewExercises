f5c094808a3186505757234da05a1f1d70733f46
Josh Hoffman <jhoffman@mobilehealth.com>
2024-07-27 12:56:38 -0400

    Added Tester exercise

---
+++ C#Exercises/Exercises/Exercise2/Tester.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercises.Exercise2
+{
+    public class Tester
+    {
+        public bool Test(int input)
+        {
+            var check = true;
+
+            if (input >= 0)
+            {
+                for (var i = 0; i < input; i++)
+                {
+                    check = !check;
+                }
+            }
+            else
+            {
+                for (var i = input; i < 0; i++)
+                {
+                    check = !check;
+                }
+            }
+
+            return check;
+        }
+    }
+}
